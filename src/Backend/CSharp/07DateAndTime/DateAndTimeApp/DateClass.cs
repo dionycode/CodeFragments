9d6045026584805a7b18161750381b3651b632a8
Diony Code <dionycode@outlook.com>
2022-09-06 22:36:00 +0800

    Extract years, weeks, and days from days

---
+++ src/Backend/CSharp/07DateAndTime/DateAndTimeApp/DateClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateAndTimeApp
+{
+    public static class DateClass
+    {
+        public static (int years, int weeks, int days) ConvertDaysToYearWeekDays(int days)
+        {
+            int _year = days / 365;
+            days = days % 365;
+
+            int _weeks = days / 7;
+            days = days % 7;
+
+            return (_year, _weeks, days);
+        }
+    }
+}
