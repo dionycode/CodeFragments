aafe63749da8a08d4537e4918c4c0b520be8fb16
Diony Code <dionycode@outlook.com>
2022-09-13 18:16:22 +0800

    Test how to use Delegate

---
+++ src/Backend/CSharp/Delegate/DelegateApp/DelegateFunctionAppClass.cs
@@ -0,0 +1,76 @@
+﻿using DelegateClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateApp
+{
+    public class DelegateFunctionAppClass
+    {
+
+        public DelegateFunctionAppClass()
+        {
+            AppItemPrices = new List<decimal>();
+        }
+
+        public List<decimal> AppItemPrices { get; set; }
+
+        public decimal MyAppFunctionOneComputeSubTotal()
+        {
+            var myDelegateFunction = new DelegateFunctionClass();
+
+            myDelegateFunction.ItemPrices = AppItemPrices;
+
+            var subTotal = myDelegateFunction.ComputeSubTotal(MyAppFunctionOneActualComputeSubTotal);
+
+            return subTotal;
+        }   
+
+        public decimal MyAppFunctionTwoComputeSubTotal()
+        {
+            var myDelegateFunction = new DelegateFunctionClass();
+
+            myDelegateFunction.ItemPrices = AppItemPrices;
+
+            var subTotal = myDelegateFunction.ComputeSubTotal(MyAppFunctionTwoActualComputeSubTotal);
+
+            return subTotal;
+        }
+
+
+
+
+        /// <summary>
+        /// 
+        /// This is the actual method that is doing the computation
+        /// It is passed as parameter to the ComputeSubTotal method in DelegateMethodClass class.
+        /// 
+        /// </summary>
+        /// <param name="itemPrices"></param>
+        /// <returns></returns>
+        private decimal MyAppFunctionOneActualComputeSubTotal(decimal[] itemPrices)
+        {
+            return itemPrices.Sum();
+        }
+
+        /// <summary>
+        /// 
+        /// This is the actual method that is doing the computation
+        /// It is passed as parameter to the ComputeSubTotal method in DelegateMethodClass class.
+        /// 
+        /// This will compute sum of prices that are greater than 5.
+        /// 
+        /// </summary>
+        /// <param name="itemPrices"></param>
+        /// <returns></returns>
+        private decimal MyAppFunctionTwoActualComputeSubTotal(decimal[] itemPrices)
+        {
+            return itemPrices.Where(o => o > 5).Sum();
+        }
+
+      
+
+    }
+}
---
+++ src/Backend/CSharp/Delegate/DelegateClassLibrary/DelegateFunctionClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateClassLibrary
+{
+    public class DelegateFunctionClass
+    {
+        public DelegateFunctionClass()
+        {
+            ItemPrices = new List<decimal>();
+        }
+
+        public List<decimal> ItemPrices { get; set; }
+
+        public decimal ComputeSubTotal(Func<decimal[], decimal> myDelegateComputeSubTotal)
+        {
+            var subTotal = myDelegateComputeSubTotal(ItemPrices.ToArray());
+
+            return subTotal;
+        }
+    }
+}
