a6f43db22da6bef094e02d0c30309e6c295e2142
Diony Code <dionycode@outlook.com>
2022-09-17 11:30:50 +0800

    Access Modifiers Demo

---
+++ src/Backend/CSharp/AccessModifiers/AccessModifiersClassLibrary/MyObjectClass.cs
@@ -0,0 +1,46 @@
+﻿namespace AccessModifiersClassLibrary
+{
+    public class MyObjectClass
+    {
+        private void MyPrivateMethod()
+        { 
+        
+        }
+
+        protected void MyProtectedMethod()
+        { 
+        
+        }
+
+        private protected void MyPrivateProtectedMethod()
+        { 
+        
+        }
+
+        internal void MyInternalMethod()
+        { 
+        
+        }
+
+        protected internal void MyProtectedInternalMethod()
+        { 
+        
+        }
+
+        public void MyPublicMethod()
+        { 
+        
+        }
+
+
+        private void MyObjectClassDemoCaller()
+        {
+            MyPrivateMethod();
+            MyProtectedMethod();
+            MyPrivateProtectedMethod();
+            MyInternalMethod();
+            MyProtectedInternalMethod();
+            MyPublicMethod();
+        }
+    }
+}
---
+++ src/Backend/CSharp/AccessModifiers/AccessModifiersClassLibrary/SameProjectClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiersClassLibrary
+{
+    public class SameProjectClass
+    {
+        private void MySameProjectDemoCaller()
+        {
+            var myObjectClass = new MyObjectClass();
+
+            myObjectClass.MyInternalMethod();
+            myObjectClass.MyProtectedInternalMethod();
+            myObjectClass.MyPublicMethod();
+        }
+    }
+
+    public class SameProjectInheritanceClass : MyObjectClass
+    {
+        private void MySameProjectDemoCaller()
+        {
+            MyProtectedMethod();
+            MyPrivateProtectedMethod();
+
+            MyInternalMethod();
+            MyProtectedInternalMethod();
+            MyPublicMethod();
+        }
+    }
+}
