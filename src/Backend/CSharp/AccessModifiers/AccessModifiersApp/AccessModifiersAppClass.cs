1177a9c7ca790e3ca24efc30326c5cd1656b3369
Diony Code <109253352+dionycode@users.noreply.github.com>
2022-10-09 05:49:41 +0800

    Development (#9)
    * Access Modifiers Demo
    
    * Additional problem solved
    
    * ReactJS + TypeScript + Webpack from scratch
    
    * Start Basic JavaScript

---
+++ src/Backend/CSharp/AccessModifiers/AccessModifiersApp/AccessModifiersAppClass.cs
@@ -0,0 +1,29 @@
+﻿using AccessModifiersClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiersApp
+{
+    public class AccessModifiersAppClass
+    {
+        public void AccessModifiersAppMethod()
+        {
+            var myObjectClass = new MyObjectClass();
+
+            myObjectClass.MyPublicMethod();
+        }
+    }
+
+    public class AccessModifiersAppInheritanceClass : MyObjectClass
+    {
+        public void AccessModifiersAppMethod()
+        {
+            MyProtectedMethod();
+            MyProtectedInternalMethod();
+            MyPublicMethod();
+        }
+    }
+}
